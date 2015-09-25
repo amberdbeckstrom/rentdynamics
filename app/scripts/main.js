585e122e6424b8bbf3736f6cca9453ae673fa5e2
Amber Beckstrom <amberdmoss@gmail.com>
2015-09-25 17:25:44 -0600

    sub menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+$(document).ready(function(){
+	
+	$('#greenheader').hide();
+	
+	
+	$('#greenmove[href="#"]').click(function(){
+		$('#greenheader').slideToggle();
+		$(this).animate({
+			opacity: 0.5
+		}, 80000, "swing", function(){
+			
+		});
+	});
+
+
+});
+
